970eed522800ad93a4176d9468fe80027cbec41c
sunilsuraci <suracisunil@gmail.com>
2022-11-24 12:26:40 +0100

    Inizio

---
+++ Lampadina.java
@@ -0,0 +1,19 @@
+public class Lampadina {
+    
+    private String stato;
+    private String interruttore;
+
+    public Lampadina(){
+        this.stato = stato;
+        this.interruttore = interruttore;
+    }
+
+    public void stato(){
+
+    }
+
+    public void click(){
+        
+    }
+
+}
---
+++ TestLampadina.java
@@ -0,0 +1,9 @@
+public class TestLampadina {
+    public static void main(String[] args){
+        
+        
+
+
+
+    }
+}
